e92768373cde6155aa5077bb6bd26f668c461cd8
hupantingxue <hupantingxue@126.com>
2014-05-17 13:40:19 +0800

    Update override msgid test case;

--- tests/test_case_override.py
+++ tests/test_case_override.py
@@ -8,7 +8,9 @@ from jpush import JPushClient
 sendno = int(time.time())
 app_key = 'dd1066407b044738b6479275'
 master_secret = '2b38ce69b1de2a7fa95706ea'
-msg_json = {"platform":"all","audience":"all", "notification":{"android":{"alert":"Pall Nandroid alert"}},"options":{"time_to_live":60, "sendno":sendno, "apns_production":True, "override_msg_id":20}}
+msg_json = '''{"platform":"all","audience":"all", "notification":{"alert":"Pall Nall alert"},"options":{"time_to_live":60, "sendno":%d, "override_msg_id":1154755988}}''' %(sendno)
+
+print msg_json
 
 jpush_client = JPushClient(app_key, master_secret)
 jpush_client.send_msg(msg_json)
