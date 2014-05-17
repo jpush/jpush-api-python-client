7bcaba1edd164b11980bc33d26a47d95d21ca73f
hupantingxue <hupantingxue@126.com>
2014-05-17 15:27:13 +0800

    Make sure the test case encoding with utf-8;

--- tests/test_case_override.py
+++ tests/test_case_override.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
+
 '''Test sending msg'''
 import time
 import sys
@@ -8,7 +9,7 @@ from jpush import JPushClient
 sendno = int(time.time())
 app_key = 'dd1066407b044738b6479275'
 master_secret = '2b38ce69b1de2a7fa95706ea'
-msg_json = {"platform":"all","audience":"all", "notification":{"alert":"Pall Nall alert"},"options":{"time_to_live":60, "sendno":sendno, "override_msg_id":1154755988}}
+msg_json = {"platform":"all","audience":"all", "notification":{"alert":u"Pall Nall alert通知"},"options":{"time_to_live":60, "sendno":sendno, "override_msg_id":1154755988}}
 
 print msg_json
 
