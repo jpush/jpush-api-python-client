d23878a5aa906981cdbfa6f2a679468c627ee60d
hupantingxue <hupantingxue@126.com>
2014-06-02 22:49:01 +0800

    API3.0 Client: update test cases;

--- tests/push/test_audience.py
+++ tests/push/test_audience.py
@@ -16,34 +16,3 @@ class TestAudience(unittest.TestCase):
 
         for selector, value, result in selectors:
             self.assertEqual(selector(value), result)
-
-    def test_invalid_device_selectors(self):
-        selectors = (
-            (jpush.device_token, 'f' * 63),
-            (jpush.device_token, 'f' * 65),
-            (jpush.device_token, '0123'),
-            (jpush.device_token, 'X' * 64),
-            (jpush.device_pin, '1234567'),
-            (jpush.device_pin, 'x' * 8),
-            (jpush.apid, 'foobar'),
-            (jpush.apid, '074e84a2-9ed9-4eee-9ca4-cc597bfdbef33'),
-            (jpush.apid, '074e84a2-9ed9-4eee-9ca4-cc597bfdbef'),
-            (jpush.wns, '074e84a2-9ed9-4eee-9ca4-cc597bfdbef'),
-            (jpush.mpns, '074e84a2-9ed9-4eee-9ca4-cc597bfdbef'),
-        )
-
-        for selector, value in selectors:
-            self.assertRaises(ValueError, selector, value)
-
-    def test_compound_selectors(self):
-        self.assertEqual(
-            jpush.or_(jpush.tag('foo'), jpush.tag('bar')),
-            {'or': [{'tag': 'foo'}, {'tag': 'bar'}]})
-
-        self.assertEqual(
-            jpush.and_(jpush.tag('foo'), jpush.tag('bar')),
-            {'and': [{'tag': 'foo'}, {'tag': 'bar'}]})
-
-        self.assertEqual(
-            jpush.not_(jpush.tag('foo')),
-            {'not': {'tag': 'foo'}})
