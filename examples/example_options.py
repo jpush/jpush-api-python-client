561dcf350bf2b19356fc6d4d8d42c615d9c305df
hupantingxue <hupantingxue@126.com>
2014-07-17 10:09:40 +0800

    Add options set example;

---
+++ examples/example_options.py
@@ -0,0 +1,10 @@
+import jpush as jpush
+from conf import app_key, master_secret
+_jpush = jpush.JPush(app_key, master_secret)
+
+push = _jpush.create_push()
+push.audience = jpush.all_
+push.notification = jpush.notification(alert="Hello, world!")
+push.platform = jpush.all_
+push.options = {"time_to_live":86400, "send no":12345,"apns_production":True}
+push.send()
