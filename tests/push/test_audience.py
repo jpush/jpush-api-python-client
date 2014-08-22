759ecbd57db3279578888d8e4450f2e518c52119
hupantingxue <hupantingxue@126.com>
2014-08-22 12:04:14 +0800

    1. Add test case; 2. Update travis run script;

--- tests/push/test_audience.py
+++ tests/push/test_audience.py
@@ -7,8 +7,8 @@ class TestAudience(unittest.TestCase):
         selectors = (
             (jpush.tag, 'tag1', {'tag': ['tag1']}),
             (jpush.tag_and, 'tag1', {'tag_and': ['tag1']}),
-            (jpush.alias, 'alias1', {'alias':'alias1'}),
-            (jpush.registration_id, 'regid1', {'registration_id':'regid1'}),
+            (jpush.alias, 'alias1', {'alias':['alias1']}),
+            (jpush.registration_id, 'regid1', {'registration_id':['regid1']}),
         )
         for selector, value, result in selectors:
             self.assertEqual(selector(value), result)
