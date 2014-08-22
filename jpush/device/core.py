72415cd461819f6d31884c906c1e7c75ea8903b4
hupantingxue <hupantingxue@126.com>
2014-08-22 11:27:33 +0800

    1. Add unit test cases; 2. Add device test core file;

---
+++ jpush/device/core.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Device(object):
+    """Device info query/update..
+
+    """
+    def __init__(self):
+        self.entity = None
+    
+
+class DeviceResponse(object):
+    """
+    """
