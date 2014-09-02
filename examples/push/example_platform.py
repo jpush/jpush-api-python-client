669eece80ff3dcdee23de3a1065f5f5cb2485161
hupantingxue <hupantingxue@126.com>
2014-09-02 17:44:59 +0800

    Modify platform message push sample;

--- examples/push/example_platform.py
+++
@@ -1,9 +0,0 @@
-import jpush as jpush
-from conf import app_key, master_secret
-_jpush = jpush.JPush(app_key, master_secret)
-
-push = _jpush.create_push()
-push.audience = jpush.all_
-push.notification = jpush.notification(alert="Hello, JPush!", ios=jpush.ios(alert="Hello, IOS JPush!", badge="+1", sound="a.caf", extras={'k1':'v1'}))
-push.platform = jpush.all_
-push.send()
