4ca440581841070aa9ea24dcf3aeaf12cca63c03
hupantingxue <hupantingxue@126.com>
2014-09-02 15:25:44 +0800

    Fix issue: python 3.x print replaced to print ();

--- jpush/device/core.py
+++ jpush/device/core.py
@@ -30,6 +30,7 @@ class Device(object):
         url = common.TAGLIST_URL
         body = None
         info = self.send("GET", url, body)
+        print (info)
 
     def get_deviceinfo(self, registration_id):
         """Get deviceinfo with registration id.
@@ -37,16 +38,15 @@ class Device(object):
         url = common.DEVICE_URL + registration_id + "/"
         body = None
         info = self.send("GET", url, body)
-        print info
+        print (info)
 
     def set_deviceinfo(self, registration_id, entity):
         """Update deviceinfo with registration id.
         """
         url = common.DEVICE_URL + registration_id + "/"
         body = json.dumps(entity)
-        print url, body
         info = self.send("POST", url, body)
-        print info
+        print (url), (body), (info)
 
     def delete_tag(self, tag, platform=None):
         """Delete registration id tag.
@@ -55,27 +55,27 @@ class Device(object):
         body = None
         if platform:
             body = platform
-        print url, body
+        print (url), (body)
         info = self.send("DELETE", url, body)
-        print info
+        print (info)
 
     def update_tagusers(self, tag, entity):
         """Add/Remove specified tag users.
         """
         url = common.TAG_URL + tag + "/"
         body = json.dumps(entity)
-        print url, body
+        print (url), (body)
         info = self.send("POST", url, body)
-        print info
+        print (info)
 
     def check_taguserexist(self, tag, registration_id):
         """Check registration id whether in tag.
         """
         url = common.TAG_URL + tag + "/exist/"
         body = registration_id
-        print url, registration_id 
+        print (url), (registration_id)
         info = self.send("GET", url, body)
-        print info
+        print (info)
 
     def delete_alias(self, alias, platform=None):
         """Delete appkey alias.
@@ -84,9 +84,9 @@ class Device(object):
         body = None
         if platform:
             body = platform
-        print url, body
+        print (url), (body)
         info = self.send("DELETE", url, body)
-        print info
+        print (info)
 
     def get_aliasuser(self, alias, platform=None):
         """Get appkey alias users.
@@ -95,9 +95,9 @@ class Device(object):
         body = None
         if platform:
             body = platform
-        print url, body
+        print (url), (body)
         info = self.send("GET", url, body)
-        print info
+        print (info)
     
 class DeviceResponse(object):
     """Response to a successful device request send.
