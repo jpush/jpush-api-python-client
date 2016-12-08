0a682d0860d7c26097f04e2a7eb13dc8ecd51f0c
fendouai <fendouai@gmail.com>
2016-12-08 23:26:26 +0800

    add a new example

--- examples/push_examples/example_alias.py
+++ examples/push_examples/example_alias.py
@@ -3,12 +3,19 @@ from conf import app_key, master_secret
 _jpush = jpush.JPush(app_key, master_secret)
 _jpush.set_logging("DEBUG")
 push = _jpush.create_push()
-
+alias=["alias1", "alias2"]
+alias1={"alias": alias}
+print alias1
+push.audience = jpush.audience(
+    jpush.tag("tag1", "tag2"),
+    alias1
+)
+'''
 push.audience = jpush.audience(
             jpush.tag("tag1", "tag2"),
             jpush.alias("alias1", "alias2")
         )
-
+'''
 
 push.notification = jpush.notification(alert="Hello world with audience!")
 push.platform = jpush.all_
