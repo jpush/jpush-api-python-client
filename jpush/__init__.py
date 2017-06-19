14e03be7541b770266288f33508333b084367ad5
Helperhaps <helperhaps@users.noreply.github.com>
2017-06-19 06:54:09 +0000

    update version

--- jpush/__init__.py
+++ jpush/__init__.py
@@ -73,7 +73,7 @@ __all__ = [
     schedulepayload,
 ]
 
-__version__ = '3.2.7'
+__version__ = '3.2.8'
 VERSION = tuple(map(int,  __version__.split('.')))
 
 # Silence urllib3 INFO logging by default
